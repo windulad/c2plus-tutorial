893ed2c4ba4e16146d967317053a5c027a5b3eb9
Windula Dissanayake <charithwinwindula@gmail.com>
2023-02-07 17:28:04 +0530

    commit

---
+++ StrFind/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string try1 = "A simple sentence";
+    int find1 = try1.find("se", 0);
+
+    cout << find1 << endl;
+    return 0;
+}
